8bd92806446a3d7a3d8719b90fcd21dc1b625d94
takenori-y <takenori.yoshimura24@gmail.com>
2022-03-15 20:41:00 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,41 +1,41 @@
 from setuptools import find_packages, setup
 
+exec(open("diffsptk/version.py").read())
 
-exec(open('diffsptk/version.py').read())
-
-with open('README.md', 'r') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name='diffsptk',
+    name="diffsptk",
     version=__version__,
-    description='Python module for speech signal processing',
-    author='SPTK Working Group',
-    author_email='takenori@sp.nitech.ac.jp',
-    url='https://github.com/sp-nitech/diffsptk',
-    download_url='',
-    packages=find_packages(exclude=('docs', 'tests', 'tools')),
+    description="Python module for speech signal processing",
+    author="SPTK Working Group",
+    author_email="takenori@sp.nitech.ac.jp",
+    url="https://github.com/sp-nitech/diffsptk",
+    download_url="",
+    packages=find_packages(exclude=("docs", "tests", "tools")),
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    keywords='dsp speech processing sptk pytorch',
-    license='Apache 2.0',
+    long_description_content_type="text/markdown",
+    keywords="dsp speech processing sptk pytorch",
+    license="Apache 2.0",
     classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.8',
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.8",
     ],
-    python_requires='>= 3.8',
+    python_requires=">= 3.8",
     install_requires=[
-        'torch >= 1.8.0',
+        "torch >= 1.8.0",
+        "numpy",
     ],
     extras_require={
-        'dev': [
-            'black',
-            'flake8',
-            'isort',
-            'numpydoc',
-            'pydata-sphinx-theme',
-            'pytest',
-            'sphinx',
+        "dev": [
+            "black",
+            "flake8",
+            "isort",
+            "numpydoc",
+            "pydata-sphinx-theme",
+            "pytest",
+            "sphinx",
         ],
     },
 )
