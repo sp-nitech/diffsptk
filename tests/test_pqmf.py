33061c5de781833a528946a0ee94082bf677995f
takenori-y <takenori.yoshimura24@gmail.com>
2022-04-17 16:59:00 +0900

    improve coverage

--- tests/test_pqmf.py
+++ tests/test_pqmf.py
@@ -23,15 +23,16 @@ import tests.utils as U
 
 @pytest.mark.parametrize("device", ["cpu", "cuda"])
 @pytest.mark.parametrize("M", [10, 11])
-def test_compatibility(device, M, K=4, T=20):
-    pqmf = diffsptk.PQMF(K, M)
+@pytest.mark.parametrize("a", [10, 50, 100])
+def test_compatibility(device, a, M, K=4, T=20):
+    pqmf = diffsptk.PQMF(K, M, alpha=a)
 
     U.check_compatibility(
         device,
         [lambda x: torch.transpose(x, 1, 2), pqmf],
         [],
         f"nrand -l {T}",
-        f"pqmf -k {K} -m {M}",
+        f"pqmf -k {K} -m {M} -a {a}",
         [],
         dx=T,
         dy=K,
