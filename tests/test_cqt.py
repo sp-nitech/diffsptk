cf4a0856b598573005ac8985038b11b251d920d0
takenori-y <takenori.yoshimura24@gmail.com>
2022-08-01 11:16:39 +0900

    add simple test

--- tests/test_cqt.py
+++ tests/test_cqt.py
@@ -15,13 +15,20 @@
 # ------------------------------------------------------------------------ #
 
 import pytest
+import torch
 
 import diffsptk
 import tests.utils as U
 
 
 @pytest.mark.parametrize("device", ["cpu", "cuda"])
-def test_differentiable(device, fp=100, sr=16000, T=1000):
-    cqt = diffsptk.ConstantQTransform(fp, sr)
+def test_compatibility(device, f_min=100, sr=16000, T=1000):
+    if device == "cuda" and not torch.cuda.is_available():
+        return
+
+    x = diffsptk.sin(T - 1, period=sr / f_min).to(device)
+    cqt = diffsptk.ConstantQTransform(T, sr, f_min=f_min).to(device)
+    X = cqt(x)
+    assert torch.argmax(X.abs()) == 0
 
     U.check_differentiable(device, cqt, [T])
