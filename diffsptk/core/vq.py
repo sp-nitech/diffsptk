9156b3e203c6afb88983a1f5fda33727b40f0b6f
takenori-y <takenori.yoshimura24@gmail.com>
2023-06-19 16:39:26 +0900

    support latest vq

--- diffsptk/core/vq.py
+++ diffsptk/core/vq.py
@@ -96,7 +96,7 @@ class VectorQuantization(nn.Module):
         if d == 1:
             x = x.unsqueeze(0)
 
-        xq, indices, loss = self.vq(x)
+        xq, indices, loss = self.vq(x.float())
 
         if d == 1:
             xq = xq.squeeze(0)
