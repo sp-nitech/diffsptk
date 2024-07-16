841396e2b52c5bf3c88d283b84636647b2e893f2
takenori-y <takenori.yoshimura24@gmail.com>
2024-07-17 01:17:09 +0900

    revert

--- tests/test_acr2csm.py
+++ tests/test_acr2csm.py
@@ -23,7 +23,7 @@ import tests.utils as U
 
 @pytest.mark.parametrize("device", ["cpu", "cuda"])
 @pytest.mark.parametrize("module", [False, True])
-def test_compatibility(device, module, M=23, L=100, B=2):
+def test_compatibility(device, module, M=25, L=100, B=2):
     if torch.get_default_dtype() != torch.double:  # pragma: no cover
         return
 
